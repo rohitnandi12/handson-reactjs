6a5611bc434b9b598c6bd7e62c83c8471e2f4737
Rohit Nandi <rohitnandi12@gmail.com>
2024-08-09 01:04:31 +0530

    Lamadev social-ui

---
+++ lamadev-youtube/social-ui/src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <Sidebar />
+      <Rightbar />
+    </>
+  );
+}
